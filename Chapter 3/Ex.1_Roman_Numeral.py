80ae30fee12bc9283cd75e3224c7445fd0da3835
swapnil-narwade <swapnilnarwade123@gmail.com>
2017-11-09 11:24:02 -0600

    exercise 1 done

---
+++ Chapter 3/Ex.1_Roman_Numeral.py
@@ -0,0 +1,23 @@
+x = int(input("Enter the number"))
+if x == 1:
+    print("I")
+elif x == 2:
+    print("II")
+elif x == 3:
+    print("III")
+elif x == 4:
+    print("IV")
+elif x == 5:
+    print("V")
+elif x == 6:
+    print("VI")
+elif x == 7:
+    print("VII")
+elif x == 8:
+    print("VIII")
+elif x == 9:
+    print("IX")
+elif x == 10:
+    print("X")
+else:
+    print("Enter the number between 1 to 10")
